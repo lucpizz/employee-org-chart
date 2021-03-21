cf683c6564b29f3199bc4a7214babe7f1924584d
lucpizz <Lucpizz@gmail.com>
2021-03-21 17:31:23 -0400

    change API employee count to 15

--- src/components/utils/api.jsx
+++ src/components/utils/api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
   getEmployees: function () {
     return axios.get(
-      'https://randomuser.me/api/?inc=picture,id,name,login,email,location&results=10&nat=us'
+      'https://randomuser.me/api/?inc=picture,id,name,login,email,location&results=15&nat=us'
     );
   },
 };
