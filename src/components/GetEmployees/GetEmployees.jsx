da9d773672894c69f1499385f43654698d115b3a
lucpizz <Lucpizz@gmail.com>
2021-03-20 22:49:09 -0400

    generated the get employees to render list on pages/index.jsx

--- src/components/GetEmployees/GetEmployees.jsx
+++ src/components/GetEmployees/GetEmployees.jsx
@@ -1,1 +1,51 @@
-// API call
+import React, { Component } from 'react';
+import api from '../../components/utils/api';
+import { DataGrid } from '@material-ui/data-grid';
+//import { XGrid } from '@material-ui/x-grid';
+
+export default class GetEmployees extends Component {
+  state = {
+    employee: [],
+  };
+
+  columns = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'firstName', headerName: 'First Name', width: 170 },
+    { field: 'lastName', headerName: 'Last Name', width: 180 },
+    { field: 'username', headerName: 'Username', width: 150 },
+    { field: 'email', headerName: 'Email', width: 250 },
+    { field: 'location', headerName: 'Location: State', width: 150 },
+  ];
+
+  componentDidMount() {
+    api
+      .getEmployees()
+      .then((res) => {
+        this.setState({
+          employee: res.data.results.map((e) => ({
+            id: e.login.uuid,
+            firstName: e.name.first,
+            lastName: e.name.last,
+            username: e.login.username,
+            email: e.email,
+            location: e.location.state,
+          })),
+        });
+        // eslint-disable-next-line
+        console.log(this.state.employee);
+      })
+      .catch((err) => {
+        err.message;
+      });
+  }
+
+  //rows = this.state.employee
+
+  render() {
+    return (
+      <>
+        <DataGrid rows={this.state.employee} columns={this.columns} />
+      </>
+    );
+  }
+}
