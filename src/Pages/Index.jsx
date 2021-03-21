1eeaf61bf4a05fdb5329bfaf1500f0dba719a673
lucpizz <Lucpizz@gmail.com>
2021-03-21 17:26:05 -0400

    added copyright tag

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import GetEmployees from '../components/GetEmployees/GetEmployees';
+import Copyright from '../components/Footer/copyright'
 
 function Index() {
   return (
@@ -9,6 +10,7 @@ function Index() {
       <div style={{ width: '100%', height: 800 }}>
         <GetEmployees />
       </div>
+      <Copyright />
     </div>
   );
 }
